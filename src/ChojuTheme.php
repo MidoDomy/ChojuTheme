56ee36ed907b537d54adcc753e6e7be18cd4ea52
Midhat Perenda <midhat.perenda97@gmail.com>
2023-10-14 11:56:42 +0200

    first commit

---
+++ src/ChojuTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace ChojuTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class ChojuTheme extends Plugin implements ThemeInterface
+{
+}
